cc631008f571589951269e29c3a78cb47c4d9175
Cameron Pierce <campierce88@gmail.com>
2016-12-04 19:41:03 -0800

    Project

---
+++ Pods/Target Support Files/Pods-ResignOnMissTextView/Pods-ResignOnMissTextView-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ResignOnMissTextView : NSObject
+@end
+@implementation PodsDummy_Pods_ResignOnMissTextView
+@end
---
+++ ResignOnMissTextView/ResignOnMissTextView.h
@@ -0,0 +1,19 @@
+//
+//  ResignOnMissTextView.h
+//  ResignOnMissTextView
+//
+//  Created by Cameron Pierce on 12/4/16.
+//  Copyright © 2016 Cameron Pierce. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ResignOnMissTextView.
+FOUNDATION_EXPORT double ResignOnMissTextViewVersionNumber;
+
+//! Project version string for ResignOnMissTextView.
+FOUNDATION_EXPORT const unsigned char ResignOnMissTextViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ResignOnMissTextView/PublicHeader.h>
+
+
